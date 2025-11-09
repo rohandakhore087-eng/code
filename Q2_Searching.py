d8bd4beaa8de5b3b49ea9050234e54d00bb61712
rohandakhore087-eng <rohandakhore087@gmail.com>
2025-11-09 16:08:21 +0530

    Update Q2_Searching.py

--- Q2_Searching.py
+++ Q2_Searching.py
@@ -1,50 +1,66 @@
-def linear(num,key):
+def linear(num, key):
     for i in range(len(num)):
         if num[i]==key:
+            print("Customer ID Found.")
             return i
-    return -1
+    print("Customer ID not found.")        
+    return -1        
+#Time Complexity= best case O(1) & worst case O(n)            
+            
+def binary(num, key):
+    low=0
+    n=len(num)-1
+    high=n
 
-def binary(num,key):
-    start=0
-    end=len(num)-1
-    while(start<=end):
-        mid=(start+end)//2
-
-        if num[mid]==key:
-            print("Customer ID is found")
+    while(low<=high):
+        mid=(low+high)//2
+        if(num[mid]==key):
+            print("Customer ID found.")
             return mid
-
-        elif num[mid]<key:
-            start=mid+1
-
+            
+        elif(num[mid]<key):
+            low=mid+1
+            
         else:
-            end=mid-1
-
-        return -1    
-
+            high=mid-1
     
-    print("Customer id not found")
+    print("Customer Id not found.")
     return -1
+#Time Complexity= best case O(1) & worst case O(log n)    
 
-num=[]
-a=int(input("Enter number of id"))
+num = []
+a = int(input("Enter the Number of Customer ID: "))
 for i in range(a):
-    n=int(input("Enter customer id"))
-    num.append(n)
-print(num)     
+    m=int(input("Enter the customer ID: "))
+    num.append(m)
+num.sort()    
+print(num)
 
-key=int(input("Enter id to search"))
+key=int(input("Enter the ID to search: "))
 
-c=input("Which method do you want linear or binary")
+c=input("Select the searching method (linear/binary): ")
 
 if c=='linear':
-    x=linear(num,key)
-    if x!=-1:
-        print("Customer id found")
-    else:
-        print("customer id not found")
+    linear(num, key)
 
 elif c=='binary':
-    binary(num,key)
+    binary(num, key)
+    
 else:
-    print("Error")
+    print("Invalid Choice")
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
